db319e6cc0dea1b756a0feeef92dd0aade901b9f
Do Suh <doyoung.suh@gmail.com>
2018-06-18 00:09:58 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Do on 6/11/18.
+//  Copyright © 2018 Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func turnFlashlightOn(_ sender: Any) {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func turnFlashlightOff(_ sender: Any) {
+        view.backgroundColor = .white
+    }
+    
+}
+
